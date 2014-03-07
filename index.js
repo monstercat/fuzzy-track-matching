0e82cf4a4f64f4d06fcc514cc6c91c9973b9d2a9
William Casarin <will@casarin.me>
2014-03-07 11:44:27 -0800

    Fix false matches with remix tracks

--- index.js
+++ index.js
@@ -7,15 +7,37 @@ fuzz.stripParens = function (s) {
 };
 
 fuzz.stripKeywords = function (s) {
-  return s.replace(/feat\.|vs\.|featuring|ft\./ig, "");
+  return s.replace(/(feat\.|vs\.|featuring|ft\.)[\s\w]+/ig, "");
 };
 
 fuzz.stripCharacters = function (s) {
   return s.replace(/[^a-zA-Z0-9\ ]/g, "");
 };
 
-function tokenize(a) {
-  a = fuzz.stripParens(a);
+function tags(tokens) {
+  return {
+    remix: ~tokens.indexOf("remix")
+  }
+}
+
+/**
+ * Checks to see if two sets of tags are compatible.
+ * Incombatible tags mean a non-match
+ *
+ * @param {Object} tag object for track a
+ * @param {Object} tag object for track b
+ * @param {Boolean} true if tags are compatible
+ * @api public
+ */
+
+function compatible(a, b) {
+  return a.remix === b.remix;
+}
+
+function tokenize(a, opts) {
+  opts = opts || {};
+  if (!opts.parens)
+    a = fuzz.stripParens(a);
   a = fuzz.stripKeywords(a);
   a = fuzz.stripCharacters(a);
   return a.toLowerCase().split(/\ +/).filter(function(t) {
@@ -25,9 +47,14 @@ function tokenize(a) {
 
 fuzz.test = function(target) {
   var targetTokens = tokenize(target);
+  var targetTags = tags(tokenize(target, { parens: true }));
 
   return function(find) {
     var findTokens = tokenize(find);
+    var findTags = tags(findTokens);
+
+    if (!compatible(targetTags, findTags))
+      return false;
 
     var matches = 0;
     findTokens.forEach(function(t) {
--- test/track.js
+++ test/track.js
@@ -6,11 +6,13 @@ var overIt = [
    'Over It feat. Dia Frampton'
  , 'Over It'
  , 'The Crystal Method - Over It (Ft. Dia Frampton)'
+ , 'Over It (feat. Dia Frampton)'
  , 'The Crystal Method'
  , 'Over It (Original Mix)'
 ];
 
 var positive = {
+ 'The Crystal Method - Over It [Free] (Preview)': overIt,
  'The Crystal Method Feat. Dia Frampton - Over It (Original Mix)': overIt,
  'The Crystal Method - Over It (feat. Dia Frampton of Meg & Dia)': overIt,
  'Some Guy - awesome Featuring artist': [
@@ -26,9 +28,13 @@ var negOverIt = [
   "Without A Cause"
 ];
 
+var negOne = ["One In A Million"]
+
 var negative = {
- 'The Crystal Method - Over It (feat. Dia Frampton of Meg & Dia)': negOverIt,
- 'The Crystal Method Feat. Dia Frampton - Over It (Original Mix)': negOverIt
+   'The Crystal Method - Over It (feat. Dia Frampton of Meg & Dia)': negOverIt
+ , 'The Crystal Method Feat. Dia Frampton - Over It (Original Mix)': negOverIt
+ , 'One In A Million (SirensCeol Remix)': negOne
+ , 'One In A Million (Jan Waterman Remix)': negOne
 };
 
 test('track artist matches', function(t) {
